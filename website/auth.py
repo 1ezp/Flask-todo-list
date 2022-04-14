adc86fd22878e3461054642a5b2bc3dc44406d51
Hikmet <hekmt111@gmail.com>
2022-04-14 14:15:41 +0300

    Flask todo list app

--- website/auth.py
+++ website/auth.py
@@ -52,6 +52,5 @@ def signup():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user,remember=True)
-            flash('Account created! ',category='success')
             return redirect(url_for('views.home'))
     return render_template('signup.html',user=current_user)
