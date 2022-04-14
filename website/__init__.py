82f90b09c69d1cc733f9ec8734f85dc613c46451
Hikmet <hekmt111@gmail.com>
2022-04-14 14:00:50 +0300

    Flask todo list app

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,8 @@ DB_NAME = 'database.db'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'to do flask app'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vrkudzfbaohiop:43ce417124bbc86b0cac0eec7cf54387e3bb726ace32bcd9fa5716e3c708390a@ec2-52-73-155-171.compute-1.amazonaws.com:5432/ddrdi730dqfa65'
     db.init_app(app)
     
     from .views import views
@@ -35,6 +36,5 @@ def create_app():
     return app
     
 def create_database(app):
-    if not path.exists('website/'+DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+    db.create_all(app=app)
+    print('Created Database!')
