f71f0f02e0b56bf455a396d015dea67ad01986b6
Hikmet <hekmt111@gmail.com>
2022-04-14 20:23:54 +0300

    fixe some errors

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'to do flask app'
     #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vrkudzfbaohiop:43ce417124bbc86b0cac0eec7cf54387e3bb726ace32bcd9fa5716e3c708390a@ec2-52-73-155-171.compute-1.amazonaws.com:5432/ddrdi730dqfa65'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nfhqphgqeyhxsi:969df064f6201abe1bee5a369489613a3a4578657f5f1658017a6a64bf664576@ec2-54-80-123-146.compute-1.amazonaws.com:5432/ddqmtqmbt400go'
     db.init_app(app)
     
     from .views import views
